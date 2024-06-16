594a82f3ca57c159fa37ec55b6f2e58f2cdce6c3
mo0dily <nastyagusevica@gmail.com>
2024-06-17 02:53:18 +0300

    Add files via upload

---
+++ 4.2.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include<math.h>
+#include<vector>
+#include<ctime>
+
+using namespace std;
+int main()
+{
+	int b = 0, c = 0;
+	srand(time(0));
+	cout << "Enter number of elements: ";
+	cin >> c;
+	vector<int> array;
+	for (int i = 0; i < c; i++) {
+		array.push_back((rand() % 200) - 100);
+		cout << array[i] << ' ';
+	}
+	cout << endl;
+	for (int i = 0; i < c; i++) {
+		if (array[i] < 0) {
+			b += array[i];
+		}
+	}
+	cout << "Sum of all the negative elements equals " << b;
+	return 0;
+}
---
+++ 4.2.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    int n;
+    srand(time(0));
+    cout << "Enter array size: ";
+    cin >> n;
+    vector<vector<int>> array(n, vector<int>(n));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            array[i][j] = (rand() % 200) - 100;
+        }
+    }
+
+    int diagonalSum = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << array[i][j] << "\t"; 
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        diagonalSum += array[i][i];
+    }
+
+    cout << "Sum of the elements on the diagonal: " << diagonalSum << endl;
+
+    return 0;
+}
